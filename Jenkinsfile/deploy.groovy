483111ed6ad2d99623531e3c9ba529acafe67060
Pranav Aggarwal <pranav.aggwl@gmail.com>
2020-03-13 03:48:09 +0000

    update indent, close stage brackets

--- Jenkinsfile/deploy.groovy
+++ Jenkinsfile/deploy.groovy
@@ -40,8 +40,10 @@ pipeline {
           userRemoteConfigs                : [[credentialsId: '1be88572-8d8e-4440-8509-45092b8a4885',
           refspec: '+refs/heads/master:refs/remotes/origin/master',
           url: "https://github.com/techagile/det_211194.git"]]])
-        }
-      }
+        } // end of dir
+      } // end of steps
+    } // end of stage
+
   } // end of stages
 } // end of pipeline
 
